d260a9f8c09335662005b96737d489b3ae25fa79
paulinhabruno <mapalurj@gmail.com>
2022-02-22 10:24:51 -0300

    continuando o projeto

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent{
